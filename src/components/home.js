0ab00a196dd2feca302ab809986afb5525b3ab14
meng  zhou <mengzhou44@mengs-MacBook-Pro.local>
2018-07-07 16:56:58 -0600

    commit

--- src/components/home.js
+++ src/components/home.js
@@ -18,14 +18,22 @@ export default (props) => {
             <main>
                 <section className="section-about">
                     <div className="u-center-text">
-                        <h2 className="heading-secondary">Exciting tours for adventous people</h2>
+                        <h2 className="heading-secondary u-margin-bottom-big">Exciting tours for adventous people</h2>
                     </div>
                     <div className="row">
                         <div className="col-1-of-2">
-                            Text
+                            <h3 className="heading-tertiary u-margin-bottom-small">You are going to fall love with the nature</h3>
+                            <p className="paragraph">
+                                ipsum dolor, sit amet consectetur adipisicing elit. Natus voluptas ea necessitatibus corrupti fugiat delectus ullam harum, minus unde aperiam minima excepturi. Enim, voluptates architecto. Magni error temporibus asperiores dolor.
+                            </p>
+                            <h3 className="heading-tertiary u-margin-bottom-small">Live adventures like you never have before</h3>
+                            <p className="paragraph">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus voluptas ea necessitatibus corrupti fugiat delectus ullam harum, minus unde aperiam minima excepturi. Enim, voluptates architecto. Magni error temporibus asperiores dolor.
+                            </p>
+                            <a href="#" className="btn-text">Learn more&rarr;</a>
                         </div>
                         <div className="col-1-of-2">
-                            Image
+
                         </div>
                     </div>
 
