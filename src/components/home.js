b1938cc2d4ef7f2933ac8915acf6b0bb0ac6fbd2
meng  zhou <mengzhou44@mengs-MacBook-Pro.local>
2018-07-07 15:17:47 -0600

    add scss

--- src/components/home.js
+++ src/components/home.js
@@ -12,8 +12,25 @@ export default (props) => {
                         <span className="heading-primary--main">Outdoors </span>
                         <span className="heading-primary--sub">is where life happens</span>
                     </h1>
+                    <a href="#" className="btn btn--white">Discover Tours</a>
                 </div>
             </div>
+            <main>
+                <section className="section-about">
+                    <div className="u-center-text">
+                        <h2 className="heading-secondary">Exciting tours for adventous people</h2>
+                    </div>
+                    <div className="row">
+                        <div className="col-1-of-2">
+                            Text
+                        </div>
+                        <div className="col-1-of-2">
+                            Image
+                        </div>
+                    </div>
+
+                </section>
+            </main>
         </div>
 
     );
